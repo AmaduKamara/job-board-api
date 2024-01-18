2d8f5649bca67fb237a5eb3c83da7dd53281f217
Amkam <amaduskamara@gmail.com>
2024-01-18 08:21:39 +0000

    Add api routes for jobs

--- server.js
+++ server.js
@@ -4,10 +4,29 @@ const app = express();
 
 const PORT = 4000;
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello, job board api server is running successfully" });
+// GET - All jobs
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ status: "success", message: "Get All Jobs" });
+});
+
+// GET - A single job
+app.get("/api/v1/jobs/:id", (req, res) => {
+  res.status(200).json({ status: "success", message: "Get Job" });
+});
+
+// POST - New jobs
+app.post("/api/v1/jobs", (req, res) => {
+  res.status(201).json({ message: "New Jobs" });
+});
+
+// PUT - Update a single job
+app.put("/api/v1/jobs/:id", (req, res) => {
+  res.status(200).json({ status: "success", message: "Updated job" });
+});
+
+// DELETE - Delete a single job
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  res.status(200).json({ status: "success", message: "Deleted job" });
 });
 
 app.listen(PORT, () => {
