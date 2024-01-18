9b9b8b4f1330315aa14b46a327763516fea316cc
Amkam <amaduskamara@gmail.com>
2024-01-18 09:58:00 +0000

    Extract all crud logics into seperate functions

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ const PORT = 4000;
 
 const jobs = [];
 
-// GET - All jobs
-app.get("/api/v1/jobs", (req, res) => {
+const getAllJobs = (req, res) => {
   res.status(200).json({ status: "success", data: jobs });
-});
+};
 
-// GET - A single job
-app.get("/api/v1/jobs/:id", (req, res) => {
+const getJob = (req, res) => {
   const id = req.params.id * 1; // multiply the returned id by 1 to convert it to string value
 
   if (id > jobs.length) {
@@ -34,18 +32,16 @@ app.get("/api/v1/jobs/:id", (req, res) => {
   // }
 
   res.status(200).json({ status: "success", data: job });
-});
+};
 
-// POST - New jobs
-app.post("/api/v1/jobs", (req, res) => {
+const createNewJob = (req, res) => {
   const job = req.body;
 
   jobs.push(job);
   res.status(201).json({ message: "Add New Jobs", job });
-});
+};
 
-// PUT - Update a single job
-app.patch("/api/v1/jobs/:id", (req, res) => {
+const updateJob = (req, res) => {
   const id = req.params.id * 1;
 
   const job = jobs.find((job) => job.id === id);
@@ -54,14 +50,27 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
       .status(404)
       .json({ status: "Failed", message: "Tour not found" });
   }
- 
   res.status(200).json({ status: "success", message: "Updated job" });
-});
+};
+
+const deleteJob = (req, res) => {
+  res.status(200).json({ status: "success", message: "Deleted job" });
+};
+
+// GET - All jobs
+app.get("/api/v1/jobs", getAllJobs);
+
+// GET - A single job
+app.get("/api/v1/jobs/:id", getJob);
+
+// POST - New jobs
+app.post("/api/v1/jobs", createNewJob);
+
+// PUT - Update a single job
+app.patch("/api/v1/jobs/:id", updateJob);
 
 // DELETE - Delete a single job
-app.delete("/api/v1/jobs/:id", (req, res) => {
-  res.status(200).json({ status: "success", message: "Deleted job" });
-});
+app.delete("/api/v1/jobs/:id", deleteJob);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
